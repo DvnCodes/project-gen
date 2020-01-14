6be6742eaa30a6e0ef9d0b0643d1d0a58e5be52e
DvnCodes <DvnCodes@users.noreply.github.com>
2020-01-14 13:01:48 +0000

    basic project creator working

--- project-gen.js
+++ project-gen.js
@@ -4,13 +4,36 @@ const projectGen = projectName => {
   fs.mkdir(`${projectName}`, err => {
     if (err) {
       throw err;
+    } else {
+      console.log("project directory created");
+      fs.writeFile(`${projectName}/index.js`, "", err => {
+        if (err) {
+          throw err;
+        } else console.log("file created inside directory");
+      });
+      fs.writeFile(`${projectName}/README.md`, "", err => {
+        if (err) {
+          throw err;
+        } else console.log("readme file created inside directory");
+      });
+      fs.writeFile(`${projectName}/\.gitignore`, "", err => {
+        if (err) {
+          throw err;
+        } else console.log("gitignore file created inside directory");
+      });
+      fs.mkdir(`${projectName}/spec`, err => {
+        if (err) {
+          throw err;
+        } else {
+          console.log("spec directory created");
+          fs.writeFile(`${projectName}/spec/index.spec.js`, "", err => {
+            if (err) {
+              throw err;
+            } else console.log("index.spec file created inside directory");
+          });
+        }
+      });
     }
-    fs.writeFile(`${projectName}/index.js`, "", err => {
-      if (err) {
-        throw err;
-      }
-      console.log("directory and file created");
-    });
   });
 };
 
